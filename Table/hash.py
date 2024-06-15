cf979c8e808a29d8467a345cbe7e258dd1f3f078
Anthony Su <anthonysu798.ca@gmail.com>
2024-06-14 22:20:40 -0400

    Updated LinkedList py

--- Table/hash.py
+++ Table/hash.py
@@ -97,3 +97,57 @@ print(haashMap.get(1))
 
 # Recursion
 # Write a code for recursion
+
+# 5 Marks out of 50
+# Ask you something and answer in plain english
+# What is quick sort for example 
+
+# 10 marks
+# Second part
+# 5 step analysis
+
+# Second part
+# Given some code, logic errors, compile errors. Fixing code
+
+# Give some function
+# Fill out some line of the function, rewrite the whole function
+
+# 15 marks - Programming part
+# Write python code for it
+# One Recursion question # 7 marks
+
+# Linked list 8 marks. Given a scenairo code it out in a linked list
+
+
+H(i) = i % 10
+
+When collision occur use this function
+H(i,j) = (H) +(j+2) % 10 - set j+2 which prevent collision
+j = probing
+
+one collision is occur so j is 1
+H(26, 1) = (6 + (1 + 2)) % 10 - |6 from the previous hash function|   -- second collision occur
+
+H(26, 2) = (6 + (2 + 2)) % 10 - | go to index of 0 which is occupied 
+
+H(26, 3) = (6 + (3 + 2)) % 10 
+
+i = 8, 9, 10, 16, 26, 36
+
+0 | 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9 | - index 
+10| 26|   |   |   |   |16 |   | 8 | 9 | - value
+
+set the probing to 2 which is j + 2
+
+36 % 10 = 6 since 6 already have an value so use the second hash function
+
+H(36, 1) = (6 + (1 + 2)) % 10 - 9 sinch 9 also have a value go agaain
+H(36, 2) = (6 + (2 + 2)) % 10 = 0 which 0 index have a value it go again
+H(36, 3) = (6 + (3 + 2)) % 10 = 2 which empty so it insert into index of 2
+
+0 | 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9 | - index 
+10| 26| 36|   |   |   |16 |   | 8 | 9 | - value
+
+h(x) = i % 10
+H(i,j) = i + j + 2 % 10 
+
---
+++ Table/recursive.py
@@ -0,0 +1,13 @@
+def factorial(number):
+    if number == 0 or number == 1
+        return number
+    else:
+        return number * factorial(number - 1)
+    
+def fiboncaci(number):
+    if number == 0
+        return 0
+    elif number == 1:
+        return 1
+    else:
+        return fiboncaci(number - 1) + fiboncaci(number - 2)
